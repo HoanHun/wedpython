6e2136ac44e7d2a7614a43ba8a1590137694fbfd
hung hoang <youremail@example.com>
2026-07-30 21:46:10 +0700

    add

--- app/models.py
+++ app/models.py
@@ -9,7 +9,9 @@ class Category(models.Model):
     is_sub = models.BooleanField(default=False, verbose_name="Là danh mục phụ")
     name = models.CharField(max_length=100, null=True, verbose_name="Tên danh mục")
     slug = models.SlugField(max_length=100, unique=True, verbose_name="Slug (Đường dẫn)")
-
+    class Meta:
+        verbose_name = "Danh mục"
+        verbose_name_plural = "Danh mục sản phẩm"
     def __str__(self):
         return self.name or ''
 class Changeform(UserCreationForm):
@@ -30,6 +32,9 @@ class Product(models.Model):
     image = models.ImageField(null=True, blank=True,verbose_name="Hình ảnh")
     digital = models.BooleanField(default=False, null=True, blank=False,verbose_name='số sản phẩm')
     detail = models.TextField(null=True,blank=True,verbose_name="Mô tả chi tiết")
+    class Meta:
+        verbose_name = "Sản phẩm"
+        verbose_name_plural = "Danh sách sản phẩm"
     def __str__(self):
         return self.name or ''
     # điều chỉnh thuộc tính 
@@ -47,6 +52,9 @@ class Order(models.Model):
     day = models.DateTimeField(auto_now_add=True, verbose_name="Ngày đặt") 
     complete = models.BooleanField(default=False, null=True, blank=False, verbose_name="Đã hoàn thành")
     transaction_id = models.CharField(max_length=100, null=True, blank=True, verbose_name="Mã giao dịch")
+    class Meta:
+        verbose_name = "Đơn hàng"
+        verbose_name_plural = "Quản lý đơn hàng"
     def __str__(self):
         return str(self.id)
     #  đếm số lượng sản phẩm 
@@ -67,6 +75,9 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=False, verbose_name="Đơn hàng")
     quantity = models.IntegerField(default=0, null=True, blank=True, verbose_name="Số lượng")
     day_added = models.DateTimeField(auto_now_add=True, verbose_name="Ngày thêm")
+    class Meta:
+        verbose_name = "Chi tiết đơn hàng"
+        verbose_name_plural = "Chi tiết đơn hàng"
 # tính tổng tiền của từng sản phẩm
     @property
     def get_total(self):
