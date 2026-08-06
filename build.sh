73fbfeb37055519a356dec7c5c88e486c09d207f
hung hoang <youremail@example.com>
2026-08-06 21:19:05 +0700

    fix data tự động

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
-
-pip install -r requirements.txt
+#Cài đặt các thư viện cần thiết (Django, PostgreSQL vân vân) lên server Render.
+pip install -r requirements.txt 
+# Gom các file giao diện (CSS, JS) để trang web và trang Admin không bị vỡ.
 python manage.py collectstatic --no-input
+# Chạy các lệnh migrate để tạo các bảng trong cơ sở dữ liệu.
 python manage.py migrate
+# Tạo tài khoản Admin để đăng nhập vào trang Admin của Django. Nếu tài khoản đã tồn tại thì bỏ qua lỗi.
+python manage.py createsuperuser --noinput || true
